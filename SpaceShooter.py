9727852398f3b1d77134fd9763c882af0995a37d
mrgerwin <jgerwin@otsegoknights.org>
2026-04-29 08:57:26 -0400

    added small hit rectangles

--- SpaceShooter.py
+++ SpaceShooter.py
@@ -5,7 +5,6 @@ import time
 from SpaceShooterSprites import *
 import sys
 
-
 def maskCollide(ship, points):
     biggest = 0
     smallest = 99999999999
@@ -44,12 +43,56 @@ def LineCollide(ship, pt1, pt2):
     else:
         return False
 
+def slope(pt1, pt2):
+    
+    num = pt2[1]-pt1[1]
+    den = pt2[0]-pt1[0]
+    
+    if den==0:
+        answer = 0
+    else:
+        answer = num/den
+    return answer
+
+def hitRect(pt1, pt2):
+    pointsOnLine = []
+    hitRects = []
+    deltax = int(abs(pt2[0]-pt1[0]))
+    for i in range(deltax):
+        pointOnLine = [pt1[0]+i, pt1[1]+i*slope(pt1, pt2)]
+        pointsOnLine.append(pointOnLine)
+    
+    for pt in pointsOnLine:
+        a_hitRect = pygame.Rect(pt[0], pt[1], 1, 1)
+        hitRects.append(a_hitRect)
+    
+    return hitRects
+
 def drawPolyLine():
     rect1 = pygame.draw.line(window, red, points[0], points[1])
     rect2 = pygame.draw.line(window, red, points[1], points[2])
     rect3 = pygame.draw.line(window, red, points[2], points[0])
     
-    return [rect1, rect2, rect3]
+    hitRects1 = hitRect(points[0], points[1])
+    hitRects2 = hitRect(points[1], points[2])
+    hitRects3 = hitRect(points[0], points[2])
+    
+    allhitRects = []
+    allhitRects=hitRects1.copy()
+    allhitRects=hitRects2.copy()
+    allhitRects=hitRects3.copy()
+    
+    for rec in hitRects1:
+        pygame.draw.rect(window, yellow, rec)
+    for rec in hitRects2:
+        pygame.draw.rect(window, blue, rec)
+        #print(rec)
+    for rec in hitRects3:
+        pygame.draw.rect(window, green, rec)
+    
+    #print(allhitRects)
+    
+    return allhitRects
 def drawText(font, text):
     label = font.render(text, True, white)
     
@@ -58,6 +101,9 @@ def drawText(font, text):
 black = [0 ,0,0]
 white = [255, 255, 255]
 red = [255, 0, 0]
+blue = [10, 10, 100]
+yellow = [255, 255, 0]
+green = [0, 100, 10]
 screen_size = [800,800]
 window = pygame.display.set_mode(screen_size)
 timer = pygame.time.Clock()
@@ -65,7 +111,7 @@ pygame.font.init()
 font = pygame.font.SysFont("consolas", 30)
 FrameNum = 0
 player = Ship(window, [400, 400])
-points = [(100, 100), (150, 150), (200, 100), (100, 100)]
+points = [(100, 100), (150, 150), (200, 100)]
 #linestriangle = pygame.draw.lines(window, red, False, points)
 #triangle = pygame.draw.polygon(window, red, points)
 
@@ -73,7 +119,7 @@ points = [(100, 100), (150, 150), (200, 100), (100, 100)]
 #theMaskImage = theMask.to_surface()
 
 lasers = []
-
+#rects = drawPolyLine()
 while True:
     FrameNum += 1
     window.fill(black)
@@ -82,7 +128,24 @@ while True:
     player.move()
     player.draw()
     rects = drawPolyLine()
-    print(maskCollide(player, points))
+    
+    for rect in rects:
+        if player.rect.colliderect(rect):
+            print("Collision")
+    
+    """NOT NEEDED IF USING RECT COLLIDE
+    #Create rects Masks
+    rectsMasks = []
+    for rect in rects:
+        rect_mask = pygame.mask.Mask((rect.width, rect.height), fill = True)
+        rectsMasks.append(rect_mask)
+    """
+    """FIX OFFSET CODE
+    for i in range(len(rectsMasks)):
+        offset = (player.rect.x-rects[i].x, player.rect.y-rects[i].y)
+        if player.mask.overlap(rectsMasks[i], offset):
+            print("collision")
+    """
     #pygame.draw.rect(window, white, linestriangle)
     #pygame.draw.polygon(window, red, points)
     #linestriangle = pygame.draw.lines(window, red, False, points)
